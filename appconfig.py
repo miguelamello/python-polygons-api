20ffebd989a52365d13aa6ba844f90bfb910fcf6
Miguel Angelo Mello] <miguelangelomello@gmail.com>
2023-07-17 02:04:12 -0300

    2023-07-17 02:04:12 by Miguel

--- appconfig.py
+++
@@ -1,7 +0,0 @@
-env = {
-  #'mongo_host': 'mongodb://localhost:27017/polygon?retryWrites=false&directConnection=true', 
-  'mongo_host': 'mongodb://root:6X4zwMMMKxIpY95IbTpbiuirUMt7DMyr@docdb-2023-07-10-22-09-24.cluster-cqom2zrpyb7i.sa-east-1.docdb.amazonaws.com:27017/polygon?ssl=true&tlsCAFile=global-bundle.pem&retryWrites=false&directConnection=true', 
-  #'redis_host': 'localhost',
-  'redis_host': 'cluster1-orionsotf-0001-001.ukaiu6.0001.sae1.cache.amazonaws.com',
-  'redis_ttl': 3600,
-}
